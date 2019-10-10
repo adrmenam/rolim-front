d4ea3be2a35b04354c8c56a391e4ae667f2f7e63
adrian.mena <adrian.mena@easysoft.com.ec>
2019-10-09 21:21:52 -0500

    return home in visit mobile component

--- src/app/pages/visit-mobile/visit-mobile.component.ts
+++ src/app/pages/visit-mobile/visit-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visit-mobile',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisitMobileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public returnHome(){
+    this.router.navigate(['index']);
   }
 
 }
